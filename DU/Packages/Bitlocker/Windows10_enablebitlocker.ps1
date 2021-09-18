b2018f2115de55b0cbe84ce0d56a5a53f67fc2da
Call4cloud <90128448+Call4cloud@users.noreply.github.com>
2021-09-18 11:14:30 +0200

    Update Windows10_enablebitlocker.ps1

--- DU/Packages/Bitlocker/Windows10_enablebitlocker.ps1
+++ DU/Packages/Bitlocker/Windows10_enablebitlocker.ps1
@@ -1,3 +1,6 @@
+##########################
+#  create bitlocker.ps###
+############################
 $content = @'
 $BLinfo = Get-Bitlockervolume
 if($BLinfo.EncryptionPercentage -ne '100' -and $BLinfo.EncryptionPercentage -ne '0'){
@@ -11,26 +14,81 @@ $BLV = Get-BitLockerVolume -MountPoint "C:" | select *
 BackupToAAD-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $BLV.KeyProtector[1].KeyProtectorId
 }
 if($BLinfo.EncryptionPercentage -eq '0'){
-Enable-BitLocker -MountPoint "C:" -EncryptionMethod XtsAes128 -UsedSpaceOnly -SkipHardwareTest -RecoveryPasswordProtector
+Enable-BitLocker -MountPoint "C:" -EncryptionMethod XtsAes256 -UsedSpaceOnly -SkipHardwareTest -RecoveryPasswordProtector
+$BLV = Get-BitLockerVolume -MountPoint "C:" | select *
+BackupToAAD-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $BLV.KeyProtector[1].KeyProtectorId
+}
+if($BLinfo.EncryptionPercentage -eq '100'){
 $BLV = Get-BitLockerVolume -MountPoint "C:" | select *
 BackupToAAD-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $BLV.KeyProtector[1].KeyProtectorId
 }
 '@
 
-Out-File -FilePath "C:\ProgramData\CustomScripts\enablebitlocker.ps1" -Encoding unicode -Force -InputObject $content
-
- 
-# create custom folder and write PS script
+##########################
+# output content to file #
+##########################
 $path = $(Join-Path $env:ProgramData CustomScripts)
 if (!(Test-Path $path))
 {
 New-Item -Path $path -ItemType Directory -Force -Confirm:$false
 }
 Out-File -FilePath $(Join-Path $env:ProgramData CustomScripts\enablebitlocker.ps1) -Encoding unicode -Force -InputObject $content -Confirm:$false
- 
-# register script as scheduled task
+
+###########################################################
+# register script as scheduled task to run at each logon  #
+###########################################################
+
 $Time = New-ScheduledTaskTrigger -AtLogOn
 $User = "SYSTEM"
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ex bypass -file `"C:\ProgramData\CustomScripts\enablebitlocker.ps1`""
 Register-ScheduledTask -TaskName "EnableBitlocker" -Trigger $Time -User $User -Action $Action -Force
+
+################################
+###Create Bitlocker Policies ###
+################################
+
+$BitLockerRegLoc = 'HKLM:\SOFTWARE\Policies\Microsoft'
+New-Item -Path "$BitLockerRegLoc" -Name 'FVE'
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'ActiveDirectoryBackup' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'RequireActiveDirectoryBackup' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'OSRequireActiveDirectoryBackup' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'OSRecovery' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'OSManageDRA' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'OSActiveDirectoryBackup' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'FDVRecovery' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'FDVManageDRA' -Value '00000000' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'FDVRecoveryPassword' -Value '00000002' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'FDVRecoveryKey' -Value '00000002' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'FDVActiveDirectoryBackup' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'FDVRequireActiveDirectoryBackup' -Value '00000001' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'OSActiveDirectoryInfoToStore' -Value '00000002' -PropertyType DWORD
+New-ItemProperty -Path "$BitLockerRegLoc\FVE" -Name 'FDVActiveDirectoryInfoToStore' -Value '00000002' -PropertyType DWORD
+
+############################################
+###Eject all Media before bitlocker	 ###
+############################################
+
+#$Diskmaster = New-Object -ComObject IMAPI2.MsftDiscMaster2 
+#$DiskRecorder = New-Object -ComObject IMAPI2.MsftDiscRecorder2 
+#$DiskRecorder.InitializeDiscRecorder($DiskMaster) 
+#$DiskRecorder.EjectMedia() 
+
+$volumes = get-wmiobject -Class Win32_Volume | where{$_.drivetype -eq '2'}  
+foreach($volume in $volumes){
+$ejectCmd = New-Object -comObject Shell.Application
+$ejectCmd.NameSpace(17).ParseName($volume.driveletter).InvokeVerb("Eject")
+} 
+################################
+###Start bitlocker encryption###
+################################
+
 Start-ScheduledTask -TaskName "EnableBitlocker"
+
+
+
+
+
+
+
+
+
